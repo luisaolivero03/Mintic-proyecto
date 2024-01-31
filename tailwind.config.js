3473716346b762218c63d390a1bd6486ab66176d
Luisa <luisaolivero03@gmail.com>
2024-01-30 22:51:36 -0500

    adelanto vista login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary: {
+          '50': '#eefbf4',
+          '100': '#d5f6e3',
+          '200': '#afebcb',
+          '300': '#7adbae',
+          '400': '#44c38d',
+          '500': '#21a872',
+          '600': '#14875b',
+          '700': '#117552',
+          '800': '#0f563d',
+          '900': '#0d4734',
+          '950': '#06281e',
+        },
+      }
     },
   },
   plugins: [],
-};
+}
