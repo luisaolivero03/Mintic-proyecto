360d0d722bedecc6069aed0d9c138ab32e7c688e
Luisa Olivero <luisaolivero03@gmail.com>
2024-02-26 22:18:15 -0500

    Hooks y horario

---
+++ src/app/dashboard/horarios/page.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const Schedule = () => {
+  return (
+
+    
+   
+
+   
+   
+    <div className='flex justify-center items-center  flex-col'>
+      
+      <h1>CALENDARIO ACADEMICO 2024</h1>
+      
+      <table className="table-fixed ">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 px-4 py-2">Hora</th>
+            <th className="border border-gray-300 px-4 py-2">Lunes</th>
+            <th className="border border-gray-300 px-4 py-2">Martes</th>
+            <th className="border border-gray-300 px-4 py-2">Miercoles</th>
+            <th className="border border-gray-300 px-4 py-2">Jueves</th>
+            <th className="border border-gray-300 px-4 py-2">Viernes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+
+          <tr>
+            <td className="border border-gray-300 px-4 py-2">9:50 am</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+            <td className="border border-gray-300 px-4 py-2">hora</td>
+            <td className="border border-gray-300 px-4 py-2">mate</td>
+            <td className="border border-gray-300 px-4 py-2">español</td>
+          </tr>
+
+
+        </tbody>
+      </table>
+    </div>
+    
+  );
+}
+
+export default Schedule;
