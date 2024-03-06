6f66ff6d856fa47f54523f73101eaaa97bd34596
Sandra Sarabia <sandramilenasarabia@gmail.com>
2024-03-05 22:53:13 -0500

    modificaciones

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -72,7 +72,7 @@ const Form = ({ variant }) => {
       <label>
         Fecha de expedición de tú documento:
         <input
-          type="text"
+          type="date"
           name="Fecha de expedición"
           className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           placeholder="Fecha de expedición"
